5621fd0b0e9d4ba2bf4e1d720ab1b6e0b131bfc5
bengal2009 <bennylin@outlook.com>
2015-06-04 16:02:26 +0800

    Initial commit

---
+++ src/Sample.java
@@ -0,0 +1,127 @@
+import org.json.JSONObject;
+
+import javax.xml.bind.DatatypeConverter;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by Lin on 2015/6/4.
+ * http://blog.csdn.net/zpf8861/article/details/32329457
+ */
+public class Sample {
+
+    private static final String serverURL = "http://vop.baidu.com/server_api";
+    private static String token = "";
+    private static final String testFileName = "C:\\Users\\Lin\\IdeaProjects\\Yuyin\\out\\production\\Yuyin\\1.amr";
+    //put your own params here
+    private static final String apiKey = "bd6kSzmqtlUaG1SEjbqR4R28";
+    private static final String secretKey = "5eaad29500bcbd35c84bf6bfac5e9190";
+    private static final String cuid = "6131442";
+
+    public static void main(String[] args) throws Exception {
+        getToken();
+        method1();
+        method2();
+    }
+
+    private static void getToken() throws Exception {
+        String getTokenURL = "https://openapi.baidu.com/oauth/2.0/token?grant_type=client_credentials" +
+                "&client_id=" + apiKey + "&client_secret=" + secretKey;
+        HttpURLConnection conn = (HttpURLConnection) new URL(getTokenURL).openConnection();
+        token = new JSONObject(printResponse(conn)).getString("access_token");
+        System.out.println(token);
+    }
+
+    private static void method1() throws Exception {
+        File pcmFile = new File(testFileName);
+        HttpURLConnection conn = (HttpURLConnection) new URL(serverURL).openConnection();
+
+        // construct params
+        JSONObject params = new JSONObject();
+        params.put("format", "amr");
+        params.put("rate", 8000);
+        params.put("channel", "1");
+        params.put("token", token);
+        params.put("cuid", cuid);
+        params.put("len", pcmFile.length());
+        params.put("speech", DatatypeConverter.printBase64Binary(loadFile(pcmFile)));
+
+        // add request header
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+
+        conn.setDoInput(true);
+        conn.setDoOutput(true);
+
+        // send request
+        DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+        wr.writeBytes(params.toString());
+        wr.flush();
+        wr.close();
+
+        printResponse(conn);
+    }
+
+    private static void method2() throws Exception {
+        File pcmFile = new File(testFileName);
+        HttpURLConnection conn = (HttpURLConnection) new URL(serverURL
+                + "?cuid=" + cuid + "&token=" + token).openConnection();
+
+        // add request header
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "audio/amr; rate=8000");
+
+        conn.setDoInput(true);
+        conn.setDoOutput(true);
+
+        // send request
+        DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+        wr.write(loadFile(pcmFile));
+        wr.flush();
+        wr.close();
+
+        printResponse(conn);
+    }
+
+    private static String printResponse(HttpURLConnection conn) throws Exception {
+        if (conn.getResponseCode() != 200) {
+            // request error
+            System.out.println(conn.getResponseCode());
+        }
+        InputStream is = conn.getInputStream();
+        BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+        String line;
+        StringBuffer response = new StringBuffer();
+        while ((line = rd.readLine()) != null) {
+            System.out.println("Have response!");
+            response.append(line);
+            response.append('\r');
+        }
+        rd.close();
+        System.out.println("Finish!");
+        return new JSONObject(response.toString()).toString(4);
+    }
+
+    private static byte[] loadFile(File file) throws IOException {
+        InputStream is = new FileInputStream(file);
+
+        long length = file.length();
+        byte[] bytes = new byte[(int) length];
+
+        int offset = 0;
+        int numRead = 0;
+        while (offset < bytes.length
+                && (numRead = is.read(bytes, offset, bytes.length - offset)) >= 0) {
+            offset += numRead;
+        }
+
+        if (offset < bytes.length) {
+            is.close();
+            throw new IOException("Could not completely read file " + file.getName());
+        }
+
+        is.close();
+        return bytes;
+    }
+}
